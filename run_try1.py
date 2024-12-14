c7a37d2cad2034914be17f6a9a1e933f4f882d6f
vitalvision24 <vitalvision24@gmail.com>
2024-12-14 11:53:59 +0530

    Update run_try1.py

--- run_try1.py
+++ run_try1.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+
+
 import numpy as np
 
 import yolov5
@@ -20,8 +22,8 @@ def return_fast_output(yolo_model, img):
         labels = results_yolo.pred[0][:, 5].tolist()
     except:
         boxes = []
-        scores_yolo = []
-        labels_yolo = []
+        scores = []
+        labels = []
 
     dic = {}
     for each in labels:
@@ -43,8 +45,7 @@ def draw_bounding_boxes_pillow(image, dic):
 
     # Create a Draw object
     draw = ImageDraw.Draw(image)
-
-    # Try to load a custom font or fall back to the default font
+     # Try to load a custom font or fall back to the default font
     try:
         font = ImageFont.truetype("arial.ttf", 16)  # Load TTF font
     except IOError:
@@ -60,10 +61,13 @@ def draw_bounding_boxes_pillow(image, dic):
 
         # Add the label and score above the bounding box
         text = f"{int(label)}: {score:.2f}"
-        text_size = draw.textsize(text, font=font)
-        text_background = [x_min, y_min - text_size[1], x_min + text_size[0], y_min]
+        text_bbox = font.getbbox(text)  # Use getbbox to calculate text size
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height = text_bbox[3] - text_bbox[1]
+
+        text_background = [x_min, y_min - text_height, x_min + text_width, y_min]
         draw.rectangle(text_background, fill="red")  # Background for better text visibility
-        draw.text((x_min, y_min - text_size[1]), text, fill="white", font=font)
+        draw.text((x_min, y_min - text_height), text, fill="white", font=font)
 
     return image
 
@@ -78,6 +82,3 @@ box_img = draw_bounding_boxes_pillow(transformed_image, temp)
 # Save the image in JPG format
 box_img.save("output_image.jpg", format="JPEG")
 print("Image saved as 'output_image.jpg' in the current directory.")
-
-
-
