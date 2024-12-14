0f8cead1927f5600812f8b6168736a1f448af745
vitalvision24 <vitalvision24@gmail.com>
2024-12-14 09:07:41 +0530

    Update run_try1.py

--- run_try1.py
+++ run_try1.py
@@ -1,39 +1,11 @@
 import matplotlib.pyplot as plt
-from PIL import Image
 import numpy as np
-import pandas as pd
-import cv2
 
-import torch
-from torch import nn, optim
-import torchvision.transforms as T
-from torchvision.utils import make_grid
-from torch.utils.data import Dataset, DataLoader
-
-import timm
-import segmentation_models_pytorch as smp
-import imutils
-from skimage.transform import ProjectiveTransform
-
-import os
-from tqdm import tqdm
-from PIL import Image
-import albumentations as A
-from sklearn.model_selection import train_test_split
-import gc
-import glob
-
-import random
 import yolov5
 from ensemble_boxes import *
-import re
-import torch.nn.functional as F
 import copy
-import os
-from PIL import Image
-from PIL import ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
-# yolo_model_det = yolov5.load('/content/drive/MyDrive/final_yolo_weights.pt')
 yolo_fast = yolov5.load('/content/drive/MyDrive/yolo_on_6_fast.pt')
 
 def return_fast_output(yolo_model, img):
